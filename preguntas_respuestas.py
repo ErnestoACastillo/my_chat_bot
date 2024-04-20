ea7393368149c0824e3cb3b7bae0591c3b26c4b6
ErnestoACastillo <Ernestoacastilloh@gmail.com>
2024-04-20 19:54:54 -0400

    "Refactored get\_response() to use a dictionary of questions and responses; removed unused functions; added main() function to run the chatbot"

---
+++ preguntas_respuestas.py
@@ -0,0 +1,35 @@
+
+preguntas_respuestas = {
+    "Hola": "¡Hola! ¿En qué puedo ayudarte?",
+    "Como estas?": "Estoy bien, gracias por preguntar. ¿Y tú?",
+    "Donde esta ubicado el itla?": "El ITLA esta en Santo Domingo, Republica Dominicana.",
+    "Donde queda el itla?": "El ITLA queda en Santo Domingo, Republica Dominicana.",
+    "Donde se encuentra el itla?": "El ITLA se encuentra en Santo Domingo, Republica Dominicana.",
+    "Donde esta el itla?": "El ITLA esta en Santo Domingo, Republica Dominicana.",
+    "Donde se situa el itla?": "El ITLA se situa en Santo Domingo, Republica Dominicana.",
+    "Como esta el itla?": "El ITLA es un lugar genial para estudiar tecnologia.",
+    "Como es el itla?": "El ITLA es una institucion increible para aprender sobre tecnologia.",
+    "Que tal el itla?": "El ITLA es genial para estudiar.",
+    "Como te encuentras?": "Me encuentro bien, gracias por preguntar. ¿En qué puedo ayudarte?",
+    "Estas bien?": "Si, estoy bien. ¿Y tu?",
+    "Como te va?": "Todo bien por aqui. ¿En qué puedo ayudarte hoy?",
+    "Que pasa?": "Nada, solo aqui listo para responder tus preguntas. ¿En qué puedo ayudarte?",
+    "Que hay?": "¡Hola! ¿En qué puedo ayudarte?",
+    "Todo bien?": "Si, todo bien. ¿Y contigo?",
+    "Cual es la ubicacion del itla?": "El ITLA esta ubicado en Santo Domingo,La caleta, Republica Dominicana.",
+    "Donde queda el itla?": "El ITLA queda en Santo Domingo, La caleta, Republica Dominicana.",
+    "Cual es la localizacion del itla?": "El ITLA se localiza en Santo Domingo, La caleta, Republica Dominicana.",
+    "Donde esta situado el itla?": "El ITLA esta situado en Santo Domingo, La caleta, Republica Dominicana.",
+    "Cual es la direccion del itla?": "La direccion del ITLA es en Santo Domingo, La caleta, Republica Dominicana.",
+    "Cuando fue fundado el itla?": "El ITLA fue fundado en el año 2000.",
+    "Quien fundo el itla?": "El ITLA fue fundado por el gobierno de la República Dominicana.",
+    "Cuantas sedes tiene el itla?": "El ITLA tiene una sede principal en Santo Domingo y en algunas provincias del pais.",
+    "Que tecnologias se enseñan en el itla?": "En el ITLA se enseñan tecnologias como programacion, redes, diseño grafico, Multimedia, Mecatronica, entre otras.",
+    "Hay becas disponibles en el itla?": "Sí, el ITLA ofrece becas para estudiantes destacados.",
+    "Cuantos estudiantes tiene el itla?": "El ITLA cuenta con miles de estudiantes en todas sus carreras.",
+    "Que idiomas se enseñan en el itla?": "El ITLA principalmente enseña en español, pero también se pueden estudiar otros idiomas como inglés.",
+    "Cuál es el lema del itla?": "El lema del ITLA es 'Construyendo Futuro, Transformando Vidas'."
+}
+
+
+
